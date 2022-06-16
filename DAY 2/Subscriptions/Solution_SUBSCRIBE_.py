efc73bf3b4e4e76360848f9a49d9dbb4ea9392ae
BhumikaChauhan07 <bhumikachauhan2002@gmail.com>
2022-06-16 23:58:18 +0530

    Day2/Codechef_problems

--- DAY 2/Subscriptions/Solution_SUBSCRIBE_.py
+++
@@ -1,8 +0,0 @@
-import math
-T = int(input())
-for i in range(0,T):
-    inp = (input())
-    L = inp.split()
-    MTC = (math.ceil(int(L[0])/6))*int(L[1])  
-    #MTC = minimum total cost incurred by the group
-    print(MTC)
