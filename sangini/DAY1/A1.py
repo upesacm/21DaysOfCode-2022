9625e07bb9a071f28dfeef19049f256b981d3187
SanginiMiharia <500094896@stu.upes.ac.in>
2022-06-16 22:34:31 +0530

    DAY1

--- sangini/DAY1/A1.py
+++ sangini/DAY1/A1.py
@@ -1,10 +1,9 @@
 # cook your dish here
-print("Hello World")
-# t=int(input())
-# for i in range(t):
-#     x,y,a=map(int,input().split())
-#     if a>=x and a<y:
-#         print("Yes")
-#     else:
-#         print("No")
+ t=int(input())
+ for i in range(t):
+     x,y,a=map(int,input().split())
+     if a>=x and a<y:
+         print("Yes")
+     else:
+         print("No")
 
