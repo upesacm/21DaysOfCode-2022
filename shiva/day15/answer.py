ee825882faf79c3fedfcc6e81796aef7f0b02f85
Muchukota-Shiva <500091913@stu.upes.ac.in>
2022-06-29 23:29:32 +0530

    day 15 of 21doc

---
+++ shiva/day15/answer.py
@@ -0,0 +1,18 @@
+# 21doc, day 15, Shiva 
+#question 1
+"""def swap_case(s):
+    res = ''
+    for ch in s:
+        if ch.isupper():
+            res += ch.lower()
+        else:
+            res += ch.upper()
+    return res
+
+if __name__ == '__main__':"""
+
+#question 2
+"""def split_and_join(line):
+    line = line.split(" ")
+    line = "-".join(line)
+    return line"""
